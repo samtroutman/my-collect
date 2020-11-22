e14fed8ab10cdde90c91c8f2f0853c3c435f2dff
Sam <sam@Sams-MacBook-Air.local>
2020-11-21 21:50:23 -0500

    commit tree 9009d48b75184d8e50dd752d8e85ce5a6301a8dd

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+    i = 0
+    collection = []
+    while i < array.length
+        collection << yield(array[i])
+        i += 1
+    end
+    languages.collect do |language|
+        return collection 
+    end
+    students.collect do |student|
+        student.split(" ").first
+    end
+end
